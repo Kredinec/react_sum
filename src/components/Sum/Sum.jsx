a2105205bfd881cf98fc9d02ffba2f68e99bb88d
Vitalii Kredinets <kredinec.v@gmail.com>
2025-06-10 17:32:28 +0300

    fix bug

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,11 +1,5 @@
 // export const Sum = () => ();
 
 export const Sum = ({ a = 0, b = 0 }) => {
-  const sum = a + b;
-
-  return (
-    <p>
-      Sum of {a} and {b} is {sum}
-    </p>
-  );
+  return <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>;
 };
